5f36b6ea29eb63b073f5883d6e7794413b24523f
porslin <118132218+porslin@users.noreply.github.com>
2023-01-31 20:56:22 +0100

    Add files via upload

---
+++ public/feature-colorpicker/script/colorpicker.js
@@ -0,0 +1,48 @@
+        
+const element1 = document.getElementById("colorBtn1");
+element1.addEventListener("click", function () {
+    document.getElementById("primary-color").style.backgroundColor = "#EC0000";
+    document.getElementById("secondary-color").style.backgroundColor = "#EC0000";
+    document.getElementById("ellipse").src = "/gfx/red-ellipse.png";
+                     
+});
+        
+const element2 = document.getElementById("colorBtn2");
+element2.addEventListener("click", function () {
+    document.getElementById("primary-color").style.backgroundColor = "#3366FF";
+    document.getElementById("secondary-color").style.backgroundColor = "#3366FF";
+    document.getElementById("ellipse").src = "/gfx/blue-ellipse.png";
+    
+});
+        
+const element3 = document.getElementById("colorBtn3");
+element3.addEventListener("click", function () {
+    document.getElementById("primary-color").style.backgroundColor = "#9E3667";
+    document.getElementById("secondary-color").style.backgroundColor = "#9E3667";
+    document.getElementById("ellipse").src = "/gfx/purple-ellipse.png";
+       
+});
+        
+const element4 = document.getElementById("colorBtn4");
+element4.addEventListener("click", function () {
+    document.getElementById("primary-color").style.backgroundColor = "#1BB3BC";
+    document.getElementById("secondary-color").style.backgroundColor = "#1BB3BC";
+    document.getElementById("ellipse").src = "/gfx/turquoise-ellipse.png";
+    
+});
+        
+const element5 = document.getElementById("colorBtn5");
+element5.addEventListener("click", function () {
+    document.getElementById("primary-color").style.backgroundColor = "#63BA68";
+    document.getElementById("secondary-color").style.backgroundColor = "#63BA68";
+    document.getElementById("ellipse").src = "/gfx/green-ellipse.png";
+
+});
+        
+const element6 = document.getElementById("colorBtn6");
+element6.addEventListener("click", function () {
+    document.getElementById("primary-color").style.backgroundColor = "#FFCC38";
+    document.getElementById("secondary-color").style.backgroundColor = "#FFCC38";
+    document.getElementById("ellipse").src = "/gfx/yellow-ellipse.png";
+
+});
